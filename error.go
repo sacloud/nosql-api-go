009a7aa63f96c18227b2471bf7467f0e58362533
Masahiro Nakagawa <repeatedly@gmail.com>
2026-03-02 15:02:16 +0900

    Remove direct api-client-go dependency
    Signed-off-by: Masahiro Nakagawa <repeatedly@gmail.com>

--- error.go
+++ error.go
@@ -3,9 +3,7 @@
 
 package nosql
 
-import (
-	client "github.com/sacloud/api-client-go"
-)
+import "github.com/sacloud/saclient-go"
 
 type Error struct {
 	msg string
@@ -33,5 +31,5 @@ func NewError(msg string, err error) *Error {
 }
 
 func NewAPIError(method string, code int, err error) *Error {
-	return &Error{msg: method, err: client.NewAPIError(code, "", err)}
+	return &Error{msg: method, err: saclient.NewError(code, "", err)}
 }
